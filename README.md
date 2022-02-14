de161f37197c0a70c54d59561d42620df4b0a07f
tomi <tomiwolfman@gmail.com>
2022-02-14 17:03:01 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# useWeb3
+
+useWeb3 is a learning platform for developers to explore and learn about Web3. Whether you’re a new dev getting your hands dirty for the first time, or a seasoned developer making the transition into the Web3 space. 
+
+Explore the latest resources, tutorials, challenges, tools, courses and boilerplates and start learning. Once you’re ready, browse the job board to land a job at some of the leading companies that work on core, open-source infrastructure, products, tools, frameworks, DAO's, etc.
+
+Explore. Learn. Build.
+
+https://www.useweb3.xyz/
+
+<!-- TABLE OF CONTENTS -->
+&nbsp;
+
+## Table of Contents
+
+- [Getting Started](#getting-started)
+
+&nbsp;
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+This is an example of how you can set up your project locally. To get a local copy up and running follow these simple example steps.
+
+1. Clone the repo
+
+```sh
+git clone with HTTPS          https://github.com/wslyvh/useWeb3.git
+git clone with SSH            git@github.com:wslyvh/useWeb3.git
+git clone with Github CLI     gh repo clone wslyvh/useWeb3
+```
+
+2. Run the project
+
+```sh
+1. yarn install      # Installs all dependencies for this project
+2. yarn dev          # Starts the project in development mode
+```
+
+&nbsp;
+
+## Environment Variables
+
+- [Plausible API Key](https://plausible.io/activate)
+- [Twitter API Key](https://developer.twitter.com/en/docs/twitter-api/getting-started/getting-access-to-the-twitter-api)
+- [Airtable API Key](https://support.airtable.com/hc/en-us/articles/219046777-How-do-I-get-my-API-key-)
+
+```sh
+# You'll need to create this file in the project's root folder
+Filename: .env
+Variables:
+    # PLAUSIBLE
+    PLAUSIBLE_API_KEY=
+
+    # Twitter
+    TWITTER_CONSUMER_KEY=
+    TWITTER_CONSUMER_SECRET=
+    TWITTER_ACCESS_TOKEN=
+    TWITTER_ACCESS_TOKEN_SECRET=
+
+    # Airtable
+    AIRTABLE_API_KEY=
+    AIRTABLE_API_BASE=
+```
---
+++ src/components/layouts/main.tsx
@@ -0,0 +1,87 @@
+import { ReactNode, useEffect, useRef, useState } from 'react'
+import styles from './main.module.scss'
+import { Sitenav } from 'components/sitenav'
+import { Link } from 'components/link'
+import { Newsletter } from 'components/newsletter'
+import Fab from 'components/fab'
+import MobileNav from 'components/mobileNav'
+import useLocalStorage from '../../hooks/useLocalStorage'
+
+type Props = {
+  title?: string
+  className?: string
+  children: ReactNode
+}
+
+export function Main(props: Props) {
+  const [isMobileNavOpen, setMobileNav] = useState(false)
+  const [className, setClassName] = useState('')
+  const [theme, setTheme] = useLocalStorage('SITE_THEME', 'light')
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  const handleCLick = () => {
+    setMobileNav((state) => !state)
+  }
+  const title = props.title ?? 'useWeb3'
+
+  useEffect(() => {
+    if (props.className) setClassName(`${styles.container} ${props.className} ${theme}`)
+  }, [props.className, theme])
+
+  return (
+    <div className={className}>
+      <aside className={styles.sitenav}>
+        <Sitenav />
+        <select
+          name="theme_switcher"
+          value={theme}
+          className={styles.themeSwitcher}
+          onChange={(e) => setTheme(e.target.value)}>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+          <option value="pantone">Pantone</option>
+          <option value="blueberry_dark">Blueberry Dark</option>
+        </select>
+      </aside>
+      <aside className={styles.mobileSitenav}>
+        <MobileNav isOpen={isMobileNavOpen} />
+
+        {isMobileNavOpen && (
+          <select
+            name="theme_switcher"
+            value={theme}
+            className={styles.themeSwitcher}
+            onChange={(e) => setTheme(e.target.value)}>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+            <option value="pantone">Pantone</option>
+            <option value="blueberry_dark">Blueberry Dark</option>
+          </select>
+        )}
+      </aside>
+      <main className={styles.content}>
+        <div className={styles.inner}>
+          <header className={styles.header}>
+            <h1>{title}</h1>
+          </header>
+
+          {/* <Donate /> */}
+
+          {props.children}
+
+          <Newsletter className={styles.newsletter} />
+
+          <footer className={styles.footer}>
+            <p>
+              Follow @ <Link href="https://twitter.com/useWeb3">useWeb3</Link>. Contribute on{' '}
+              <Link href="https://github.com/wslyvh/useWeb3">Github</Link>.
+            </p>
+            <p>
+              Created by <Link href="https://twitter.com/wslyvh">@wslyvh</Link>.
+            </p>
+          </footer>
+          <Fab onClick={handleCLick} />
+        </div>
+      </main>
+    </div>
+  )
+}
